14bc043521fe104a8cfe716a9ae73dfccb31a64b
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2019-11-24 20:01:33 +0200

    Hamster

---
+++ Hamster/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hamster
+{
+    class Animal
+    {
+        public string Name;
+        public int Age = 0;
+        public double Weight;
+        public int Lifespan = 0;
+
+        public void PrintBaseInfo()
+        {
+            Console.WriteLine($"Your Hamster name is {Name} and it is {Age} year(s) old.");
+            Console.WriteLine($"Your pet's weight is {Weight}.");
+            Console.WriteLine($"It's lifespan is {Lifespan}.");
+        }
+        public void Eat()
+        {
+            Weight += 0.2; //kaal suureneb 0.2 võrra
+        }
+
+        public void Exercise()
+        {
+            Weight -= 0.1; //kaal väheneb 0.2 võrra
+        }
+
+    }
+
+
+
+
+
+
+}
---
+++ Hamster/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hamster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal hamster = new Animal();//loome objekti hamster
+            hamster.Name = "Tiki";
+            hamster.Weight = 0.3;
+            
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                hamster.Lifespan++;
+                hamster.Age++;
+
+                if (hamster.Weight <= 0.7)
+                {
+                    hamster.Eat();
+                    Console.WriteLine($"Hamster gains weight {hamster.Weight}");
+                }
+                else
+                {
+                    hamster.Exercise();
+                    Console.WriteLine($"Hamster looses weight {hamster.Weight}");
+                } 
+            }
+
+            hamster.PrintBaseInfo();
+
+            if (hamster.Lifespan == 10)
+            {
+                Console.WriteLine($"Your hamster {hamster.Name} are dead, we can't help it.");
+                hamster = null;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
